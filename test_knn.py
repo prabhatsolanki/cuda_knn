28cf2717c22219f5a6537cbba4db70cbc853ddd5
Prabhat Solanki <prabhat.solanki@cern.ch>
2025-03-03 08:01:13 +0100

    added fastgraph for comparison

--- test_knn.py
+++ test_knn.py
@@ -2,7 +2,7 @@
 import torch
 import time
 from typing import Tuple
-from pykeops.torch import LazyTensor
+from fastgraphcompute import binned_select_knn
 
 torch.ops.load_library("./knn_cuda.so")
 print("Registered knn_cuda operator:", torch.ops.knn_cuda)
@@ -11,6 +11,11 @@ print("Registered knn_cuda operator:", torch.ops.knn_cuda)
 def knn_cuda_wrapper(query: torch.Tensor, database: torch.Tensor, k: int) -> Tuple[torch.Tensor, torch.Tensor]:
     return torch.ops.knn_cuda.knn_cuda(query, database, k)
 
+
+def knn_fastgraphcompute(x: torch.Tensor, k: int)-> Tuple[torch.Tensor, torch.Tensor]:
+    row_splits = torch.tensor([0, x.shape[0]], dtype=torch.int32, device=x.device)
+    neighbor_idx, distsq = binned_select_knn(k, x, row_splits)
+    return distsq, neighbor_idx
 
 def benchmark(func, *args, **kwargs):
     if torch.cuda.is_available():
@@ -31,11 +36,18 @@ if __name__ == '__main__':
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
     N = 7000
-    D = 32
+    D = 16
     k = 80
 
     query = torch.randn(N, D, device=device)
     database = query
+
+    for _ in range(3):
+        knn_cuda_wrapper(query, database, k)
+        knn_fastgraphcompute(query, k)
+        if device == "cuda":
+            torch.cuda.synchronize()
+
 
     '''query = torch.tensor([
         [0.0, 0.0],
@@ -50,18 +62,25 @@ if __name__ == '__main__':
         [1.0, 3.0],
     ], dtype=torch.float, device=device)
     database = query
-    k = 10'''
+    k = 5'''
     
     print("\n--- Custom CUDA kNN Operator ---")
     (distances_cuda, indices_cuda), time_cuda = benchmark(knn_cuda_wrapper, query, database, k)
     print(f"\nknn_cuda -- Time: {time_cuda:.4f} s")
     print("  Distances shape:", distances_cuda.shape)
 
+    print("\n---  Distances (first query) ---")
+    print("Custom CUDA kNN distances:")
+    print(distances_cuda[:1].cpu(), indices_cuda[:1].cpu() )
 
-   
-    #print("\n--- Example Distances (first 5 rows) ---")
-    #print("Custom CUDA kNN distances (CPU):")
-    #print(distances_cuda[:5].cpu(),indices_cuda[:5].cpu() )
+    print("\n--- FastGraph---")
+    (distances_cuda, indices_cuda), time_cuda = benchmark(knn_fastgraphcompute, query, k)
+    print(f"\n FastGraph -- Time: {time_cuda:.4f} s")
+    print("  Distances shape:", distances_cuda.shape)
+
+    print("\n---  Distances (first query) ---")
+    print("FastGraph kNN distances:")
+    print(distances_cuda[:1].cpu(), indices_cuda[:1].cpu() )
 
    
     print("\n--- TorchScript Compatibility Check ---")
@@ -71,7 +90,7 @@ if __name__ == '__main__':
         print("knn_cuda operator: TorchScript Compatible")
         (distances_loaded, indices_loaded), time_loaded = benchmark(loaded_knn, query, database, k)
         print(f"\nLoaded knn_cuda -- Time: {time_loaded:.4f} s")
-        #print("Loaded knn_cuda distances (first 5 rows):")
-        #print(distances_loaded[:5].cpu())
+        print("Loaded knn_cuda distances (first query):")
+        print(distances_loaded[:1].cpu())
     except Exception as e:
         print("knn_cuda operator: TorchScript Incompatible", e)
